4ce060e0a06ae8c6ad755bced0e2faa463d21be1
ZhuoxuanZhangHarry <zz3052@columbia.edu>
2023-11-17 21:16:40 +0000

    ok model

--- DRAM_reconstruct.py
+++ DRAM_reconstruct.py
@@ -4,7 +4,7 @@ from typing import Iterable, Optional
 
 import torch
 import torch.nn as nn
-
+import PIL
 from timm.data import Mixup
 from timm.utils import accuracy
 from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
@@ -44,44 +44,89 @@ import models_vit
 import models_mae
 
 def get_args_parser():
-    parser = argparse.ArgumentParser('MAE pre-training', add_help=False)
+    parser = argparse.ArgumentParser('MAE fine-tuning for image classification', add_help=False)
     parser.add_argument('--batch_size', default=64, type=int,
                         help='Batch size per GPU (effective batch size is batch_size * accum_iter * # gpus')
-    parser.add_argument('--epochs', default=400, type=int)
+    parser.add_argument('--epochs', default=50, type=int)
     parser.add_argument('--accum_iter', default=1, type=int,
                         help='Accumulate gradient iterations (for increasing the effective batch size under memory constraints)')
 
     # Model parameters
-    parser.add_argument('--model', default='mae_vit_large_patch16', type=str, metavar='MODEL',
+    parser.add_argument('--model', default='vit_large_patch16', type=str, metavar='MODEL',
                         help='Name of model to train')
 
     parser.add_argument('--input_size', default=224, type=int,
                         help='images input size')
 
-    parser.add_argument('--mask_ratio', default=0.75, type=float,
-                        help='Masking ratio (percentage of removed patches).')
+    parser.add_argument('--drop_path', type=float, default=0.1, metavar='PCT',
+                        help='Drop path rate (default: 0.1)')
 
+    # Optimizer parameters
+    parser.add_argument('--clip_grad', type=float, default=None, metavar='NORM',
+                        help='Clip gradient norm (default: None, no clipping)')
+    parser.add_argument('--weight_decay', type=float, default=0.05,
+                        help='weight decay (default: 0.05)')
     parser.add_argument('--norm_pix_loss', action='store_true',
                         help='Use (per-patch) normalized pixels as targets for computing loss')
     parser.set_defaults(norm_pix_loss=False)
-
-    # Optimizer parameters
-    parser.add_argument('--weight_decay', type=float, default=0.05,
-                        help='weight decay (default: 0.05)')
-
     parser.add_argument('--lr', type=float, default=None, metavar='LR',
                         help='learning rate (absolute lr)')
     parser.add_argument('--blr', type=float, default=1e-3, metavar='LR',
                         help='base learning rate: absolute_lr = base_lr * total_batch_size / 256')
-    parser.add_argument('--min_lr', type=float, default=0., metavar='LR',
+    parser.add_argument('--layer_decay', type=float, default=0.75,
+                        help='layer-wise lr decay from ELECTRA/BEiT')
+
+    parser.add_argument('--min_lr', type=float, default=1e-6, metavar='LR',
                         help='lower lr bound for cyclic schedulers that hit 0')
 
-    parser.add_argument('--warmup_epochs', type=int, default=40, metavar='N',
+    parser.add_argument('--warmup_epochs', type=int, default=5, metavar='N',
                         help='epochs to warmup LR')
+
+    # Augmentation parameters
+    parser.add_argument('--color_jitter', type=float, default=None, metavar='PCT',
+                        help='Color jitter factor (enabled only when not using Auto/RandAug)')
+    parser.add_argument('--aa', type=str, default='rand-m9-mstd0.5-inc1', metavar='NAME',
+                        help='Use AutoAugment policy. "v0" or "original". " + "(default: rand-m9-mstd0.5-inc1)'),
+    parser.add_argument('--smoothing', type=float, default=0.1,
+                        help='Label smoothing (default: 0.1)')
+
+    # * Random Erase params
+    parser.add_argument('--reprob', type=float, default=0.25, metavar='PCT',
+                        help='Random erase prob (default: 0.25)')
+    parser.add_argument('--remode', type=str, default='pixel',
+                        help='Random erase mode (default: "pixel")')
+    parser.add_argument('--recount', type=int, default=1,
+                        help='Random erase count (default: 1)')
+    parser.add_argument('--resplit', action='store_true', default=False,
+                        help='Do not random erase first (clean) augmentation split')
+
+    # * Mixup params
+    parser.add_argument('--mixup', type=float, default=0,
+                        help='mixup alpha, mixup enabled if > 0.')
+    parser.add_argument('--cutmix', type=float, default=0,
+                        help='cutmix alpha, cutmix enabled if > 0.')
+    parser.add_argument('--cutmix_minmax', type=float, nargs='+', default=None,
+                        help='cutmix min/max ratio, overrides alpha and enables cutmix if set (default: None)')
+    parser.add_argument('--mixup_prob', type=float, default=1.0,
+                        help='Probability of performing mixup or cutmix when either/both is enabled')
+    parser.add_argument('--mixup_switch_prob', type=float, default=0.5,
+                        help='Probability of switching to cutmix when both mixup and cutmix enabled')
+    parser.add_argument('--mixup_mode', type=str, default='batch',
+                        help='How to apply mixup/cutmix params. Per "batch", "pair", or "elem"')
+
+    # * Finetuning params
+    parser.add_argument('--finetune', default='',
+                        help='finetune from checkpoint')
+    parser.add_argument('--global_pool', action='store_true')
+    parser.set_defaults(global_pool=True)
+    parser.add_argument('--cls_token', action='store_false', dest='global_pool',
+                        help='Use class token instead of global pool for classification')
 
     # Dataset parameters
     parser.add_argument('--data_path', default='/datasets01/imagenet_full_size/061417/', type=str,
                         help='dataset path')
+    parser.add_argument('--nb_classes', default=1000, type=int,
+                        help='number of the classification types')
 
     parser.add_argument('--output_dir', default='./output_dir',
                         help='path where to save, empty for no saving')
@@ -95,6 +140,10 @@ def get_args_parser():
 
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
                         help='start epoch')
+    parser.add_argument('--eval', action='store_true',
+                        help='Perform evaluation only')
+    parser.add_argument('--dist_eval', action='store_true', default=False,
+                        help='Enabling distributed evaluation (recommended during training for faster monitor')
     parser.add_argument('--num_workers', default=10, type=int)
     parser.add_argument('--pin_mem', action='store_true',
                         help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
@@ -110,6 +159,33 @@ def get_args_parser():
                         help='url used to set up distributed training')
 
     return parser
+def save_image(image_array, file_name):
+    """
+    Save an image from a numpy array.
+
+    :param image_array: Numpy array representing the image.
+    :param file_name: File name or path where the image will be saved.
+    """
+    # Convert to a PIL Image
+    image = PIL.Image.fromarray((image_array * 255).astype(np.uint8))
+
+    # Save the image
+    image.save(file_name)
+
+# Example usage:
+# Assuming y[0] is a numpy array representing an image
+
+def run_one_image(img, model):
+    x = torch.tensor(img)
+    print('x shape 1', x.shape)
+    # make it a batch-like
+    x = x.unsqueeze(dim=0)
+    print('x shape 2', x.shape)
+    # run MAE
+    print("x", x.shape)
+    loss, y, mask = model(x.float(), mask_ratio=0.75)
+    print("y shape: ", y.shape)
+    print("loss: ", loss)
 
 def load_classifier_model(checkpoint_path, pretrained_model):
     if checkpoint_path:
@@ -121,25 +197,14 @@ def load_classifier_model(checkpoint_path, pretrained_model):
             print("=> no checkpoint found at '{}'".format(checkpoint_path))
     return pretrained_model
 
-def get_labels(images, bool_masked_pos, device: torch.device, normlize_target: bool = True, patch_size: int = 16):
-    # import pdb; pdb.set_trace()
-    with torch.no_grad():
-        # calculate the predict label
-        mean = torch.as_tensor(IMAGENET_DEFAULT_MEAN).to(device)[None, :, None, None]
-        std = torch.as_tensor(IMAGENET_DEFAULT_STD).to(device)[None, :, None, None]
-        unnorm_images = images * std + mean  # in [0, 1]
-
-    if normlize_target:
-        images_squeeze = rearrange(unnorm_images, 'b c (h p1) (w p2) -> b (h w) (p1 p2) c', p1=patch_size, p2=patch_size)
-        images_norm = (images_squeeze - images_squeeze.mean(dim=-2, keepdim=True)
-                    ) / (images_squeeze.var(dim=-2, unbiased=True, keepdim=True).sqrt() + 1e-6)
-        # we find that the mean is about 0.48 and standard deviation is about 0.08.
-        images_patch = rearrange(images_norm, 'b n p c -> b n (p c)')
-    else:
-        images_patch = rearrange(unnorm_images, 'b c (h p1) (w p2) -> b (h w) (p1 p2 c)', p1=patch_size, p2=patch_size)
-    B, _, C = images_patch.shape
-    labels = images_patch[bool_masked_pos].reshape(B, -1, C)
-    return labels
+def prepare_model(chkpt_dir, arch='mae_vit_large_patch16'):
+    # build model
+    model = getattr(models_mae, arch)()
+    # load model
+    checkpoint = torch.load(chkpt_dir, map_location='cpu')
+    msg = model.load_state_dict(checkpoint['model'], strict=False)
+    print(msg)
+    return model
 
 @torch.no_grad()
 def evaluate_DRAM(mae_model, data_loader, device):
@@ -167,21 +232,14 @@ def evaluate_DRAM(mae_model, data_loader, device):
     # future TODO: use vector instead of for loop?
     for imgs, target in metric_logger.log_every(data_loader, 10, header):
         imgs = imgs.to(device, non_blocking=True)
+        # [batch, Channel=3, Height, Width]
         target = target.to(device, non_blocking=True)
-        # get labels TODO: why?
-        # labels = get_labels(images, bool_masked_pos, device, normlize_target, patch_size)
-        # print("labels: ", labels)
-    
-        # calculate loss
-        with torch.cuda.amp.autocast():
-            # TODO: is this mse loss?
-            loss, _, _ = mae_model(imgs, mask_ratio=0.75)
-        
-        loss_value = loss.item()
-        
-        loss_lst.append(loss_value)
 
         with torch.cuda.amp.autocast():
+            loss, pred, mask = mae_model(imgs, mask_ratio=0.75)
+            # mse loss over patches
+            # [batch, patch_num, patch_size**2 * C]
+
             rec_imgs = imgs
 
             ## TODO: perform patch attack on imgs
@@ -206,14 +264,8 @@ def main(args):
     cudnn.benchmark = True
 
     # load data
-    # simple augmentation
-    transform_train = transforms.Compose([
-            transforms.RandomResizedCrop(args.input_size, scale=(0.2, 1.0), interpolation=3),  # 3 is bicubic
-            transforms.RandomHorizontalFlip(),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
-    
-    dataset = datasets.ImageFolder(os.path.join(args.data_path, 'train'), transform=transform_train)
+    dataset = build_dataset(is_train=False, args=args)
+
     sampler = torch.utils.data.SequentialSampler(dataset)
 
     data_loader = torch.utils.data.DataLoader(
@@ -221,46 +273,19 @@ def main(args):
         batch_size=args.batch_size,
         num_workers=args.num_workers,
         pin_memory=args.pin_mem,
-        drop_last=True
+        drop_last=False
     )
 
     # create mae_model
-    # mae_model = models_vit.__dict__[args.model](
-    #     num_classes=args.nb_classes,
-    #     drop_path_rate=args.drop_path,
-    #     global_pool=args.global_pool,
-    # )
-    mae_model = models_mae.__dict__[args.model](norm_pix_loss=args.norm_pix_loss)
+    # TODO: visualize for debugging purpose
+    chkpt_dir = "mae_visualize_vit_large.pth"
+    mae_model = prepare_model(chkpt_dir, 'mae_vit_large_patch16')
     mae_model.to(device)
 
     # config parameters
     model_without_ddp = mae_model
+    print('Model loaded.')
     print("Model = %s" % str(model_without_ddp))
-
-    eff_batch_size = args.batch_size * args.accum_iter * misc.get_world_size()
-    
-    if args.lr is None:  # only base_lr is specified
-        args.lr = args.blr * eff_batch_size / 256
-
-    print("base lr: %.2e" % (args.lr * 256 / eff_batch_size))
-    print("actual lr: %.2e" % args.lr)
-
-    print("accumulate grad iterations: %d" % args.accum_iter)
-    print("effective batch size: %d" % eff_batch_size)
-
-    if args.distributed:
-        mae_model = torch.nn.parallel.DistributedDataParallel(mae_model, device_ids=[args.gpu], find_unused_parameters=True)
-        model_without_ddp = mae_model.module
-    
-    # following timm: set wd as 0 for bias and norm layers
-    param_groups = optim_factory.add_weight_decay(model_without_ddp, args.weight_decay)
-    optimizer = torch.optim.AdamW(param_groups, lr=args.lr, betas=(0.9, 0.95))
-    print(optimizer)
-    loss_scaler = NativeScaler()
-
-    # load model
-    misc.load_model(args=args, model_without_ddp=model_without_ddp, optimizer=optimizer, loss_scaler=loss_scaler)
-
 
     # eval
     test_stats = evaluate_DRAM(mae_model, data_loader, device)
